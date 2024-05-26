c3b74b998724393009e5f880f33434393a3ea4fb
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-05-26 21:54:40 +0200

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -4,3 +4,9 @@ variable "terraform_state_bucket_name" {
     default = "eks-application-state-bucket"
 }
 
+variable "vpc_id" {
+    type = string
+    description = "Cluster vpc id"
+    default = "vpc-09c6113419b39f33b"
+}
+
