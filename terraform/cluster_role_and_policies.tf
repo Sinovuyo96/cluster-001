42f49170bf6148c5a2449a8b02e106bd820a237a
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-05-26 21:41:26 +0200

    Update cluster_role_and_policies.tf

--- terraform/cluster_role_and_policies.tf
+++ terraform/cluster_role_and_policies.tf
@@ -1,32 +1,111 @@
-#####################
-# terraform State   #
-#####################
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.terraform_state_bucket_name
-  force_destroy = true
-  versioning {
-    enabled = true
-  }
- }
+##############
+# EKS Policy #
+##############
+resource "aws_iam_policy" "eks_policy" {
+  policy      = data.aws_iam_policy_document.eks_iam_policy_doc.json
+  name        = "eks_policy"
+  path        = "/"
+  description = "eks policy"
+}
+############
+# EKS ROLE #
+############
 
-# Add bucket encryption to hide sensitive state data
-resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption" {
-  bucket = aws_s3_bucket.terraform_state.bucket
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
+resource "aws_iam_role" "eks_cluster_and_eks_nodes_role" {
+  assume_role_policy = data.aws_iam_policy_document.iam_role_trust_policy.json
+  name = "cluster-role"
+
+  tags = {
+    tag-key = "dev"
+  }
+}
+
+
+data "aws_iam_policy_document" "iam_role_trust_policy" {
+  
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      identifiers = ["ec2.amazonaws.com", "eks.amazonaws.com"]
+      type        = "Service"
     }
   }
 }
 
-# resource "aws_s3_bucket_acl" "bucket_acl" {
-#   bucket = aws_s3_bucket.terraform_state.id
-#   acl    = "private"
-# }
+#######################
+# Policay attachement #
+#######################
+resource "aws_iam_role_policy_attachment" "attach_eks_role" {
+  role       = aws_iam_role.eks_cluster_and_eks_nodes_role.name
+  policy_arn = aws_iam_policy.eks_policy.arn
+}
 
-resource "aws_s3_bucket_versioning" "bucket_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
+data "aws_iam_policy_document" "eks_iam_policy_doc" {
+  statement {
+    sid = "CWLogGroupManagement"
+    actions = [
+                "logs:CreateLogGroup",
+                "logs:DescribeLogGroups",
+                "logs:PutRetentionPolicy"
+            ]
+    resources = [
+    "arn:aws:logs:*:*:log-group:/eks/",
+    "arn:aws:logs:*:*:log-group:/eks/*",
+    ]
   }
+  statement {
+    sid = "CWLogStreamManagement"
+    
+    actions = [
+               "logs:CreateLogStream",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents",
+            ]
+    resources = [
+    "arn:aws:logs:*:*:log-group:/aws/eks/*:log-stream:*",
+    "arn:aws:logs:*:*:log-group:/eks/:*"
+    ]
+  }
+  statement {
+    sid = "ec2Permissions"
+    
+    actions = [
+            "ec2:*",
+            "autoscaling:*",
+            "ec2:*",
+            "kms:*",
+            "elasticloadbalancing:*"
+            ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_and_eks_nodes_role.name
+}
+
+# Optionally, enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_cluster_and_eks_nodes_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_cluster_and_eks_nodes_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_cluster_and_eks_nodes_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_cluster_and_eks_nodes_role.name
 }
