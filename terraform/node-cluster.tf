acf031126d2d194b04073e6476317471cb56f9de
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-05-26 22:24:13 +0200

    Update node-cluster.tf

--- terraform/node-cluster.tf
+++ terraform/node-cluster.tf
@@ -3,7 +3,7 @@
 ########################
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.rancher-cluster.name
-  node_group_name = "node-js-group"
+  node_group_name = "node-group-001"
   node_role_arn   = aws_iam_role.eks_cluster_and_eks_nodes_role.arn
   subnet_ids = ["subnet-073b05c3e735dba6b", "subnet-05222976c49e763cb", "subnet-048287d7fc8d46dd4"]
 #  subnet_ids      = aws_subnet.example[*].id
