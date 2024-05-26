e7936e876dc0bc43c975ab0cd0e31eebdbed9048
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-05-26 21:39:42 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,7 +5,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "ecs-application-state-bucket"
+    bucket         = "eks-application-state-bucket"
     key            = "eks/cluster-001/terraform.tfstate"
     dynamodb_table = "terraform-aws-state-lock"
     region         = "us-east-1"
