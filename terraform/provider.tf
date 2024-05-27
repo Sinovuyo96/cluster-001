199e7f2ceacf8159dc46c461f25a62ecbc57aa35
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-05-27 07:19:19 +0200

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,12 +3,12 @@ provider aws {
 }
 
 provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "current-context"
+#  config_path    = "~/.kube/config"
+#  config_context = "current-context"
 }
 
-resource "kubernetes_namespace" "nginx-ns" {
-  metadata {
-    name = "nginx"
-  }
-}
+#resource "kubernetes_namespace" "nginx-ns" {
+#  metadata {
+#    name = "nginx"
+#  }
+#}
