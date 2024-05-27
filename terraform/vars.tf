0e94c7f928ece4228e9dfbf7e9b249008e047360
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-05-27 06:34:23 +0200

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -3,4 +3,8 @@ variable "vpc_id" {
     description = "Cluster vpc id"
     default = "vpc-09c6113419b39f33b"
 }
-
+variable "subnet_ids" {
+    type = string
+    description = "Cluster vpc id"
+    default = ["subnet-000777ab5c32b809b", "subnet-06410aafa39545e98"]
+}
