13f0b47475dfe5a02937a74170be8ceb8d202b66
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-05-28 19:32:24 +0200

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,8 +1,21 @@
+####################################
+# EKS networking config
+####################################
 variable "vpc_id" {
     type = string
     description = "Cluster vpc id"
     default = "vpc-0dc821471afbecd3b"
 }
+
+variable "subnet_ids" {
+    type = list(string)
+    description = "Cluster vpc id"
+    default = ["subnet-045ca9fc7f1dfe6f7", "subnet-0776b475ba50f5ef3"]
+}
+
+#########################
+# Cluster settings
+#########################
 variable "subnet_ids" {
     type = list(string)
     description = "Cluster vpc id"
