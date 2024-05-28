c6140b6c93f59ace14f9c7329b186b5c8a6cbc61
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-05-28 19:55:17 +0200

    Update serviceAccounts.tf

--- terraform/serviceAccounts.tf
+++ terraform/serviceAccounts.tf
@@ -1,18 +1,18 @@
-resource "kubernetes_service_account" "aws_alb_controller_svc_acc" {
-  metadata {
-    name = "aws-load-balancer-controller"
-  }
-}
+# resource "kubernetes_service_account" "aws_alb_controller_svc_acc" {
+#   metadata {
+#     name = "aws-load-balancer-controller"
+#   }
+# }
 
-resource "kubernetes_secret" "example" {
-  metadata {
-    annotations = {
-      "kubernetes.io/service-account.name" = kubernetes_service_account.aws_alb_controller_svc_acc.metadata.0.name
-    }
+# resource "kubernetes_secret" "example" {
+#   metadata {
+#     annotations = {
+#       "kubernetes.io/service-account.name" = kubernetes_service_account.aws_alb_controller_svc_acc.metadata.0.name
+#     }
 
-    generate_name = "aws-alb-ingress-svc-account-secret"
-  }
+#     generate_name = "aws-alb-ingress-svc-account-secret"
+#   }
 
-  type                           = "kubernetes.io/service-account-token"
-  wait_for_service_account_token = true
-}
+#   type                           = "kubernetes.io/service-account-token"
+#   wait_for_service_account_token = true
+# }
