9db0c7528d730d7ba42198bb7e01a9c322b029cb
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-05-28 19:33:57 +0200

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -4,20 +4,19 @@
 variable "vpc_id" {
     type = string
     description = "Cluster vpc id"
-    default = "vpc-0dc821471afbecd3b"
+    default = "	vpc-099bf61dcb2e90e0e"
 }
 
 variable "subnet_ids" {
     type = list(string)
     description = "Cluster vpc id"
-    default = ["subnet-045ca9fc7f1dfe6f7", "subnet-0776b475ba50f5ef3"]
+    default = ["subnet-05135e5929e9135e3", "subnet-0853a15f31d55f736"]
 }
 
 #########################
 # Cluster settings
 #########################
-variable "subnet_ids" {
-    type = list(string)
-    description = "Cluster vpc id"
-    default = ["subnet-045ca9fc7f1dfe6f7", "subnet-0776b475ba50f5ef3"]
+variable "cluster_name" {
+    type = string
+    default = "cluster-001"
 }
