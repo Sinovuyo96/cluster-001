9b2a9ae6773c8dd3b8460a63d7ead270e3d33767
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-05-30 20:01:52 +0200

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -4,7 +4,7 @@
 variable "vpc_id" {
     type = string
     description = "Cluster vpc id"
-    default = "	vpc-099bf61dcb2e90e0e"
+    default = "vpc-099bf61dcb2e90e0e"
 }
 
 variable "subnet_ids" {
@@ -20,3 +20,27 @@ variable "cluster_name" {
     type = string
     default = "cluster-001"
 }
+
+variable "node_group_name" {
+    type = string
+    default = "node-group-001"
+} 
+
+#######################################
+# aws alb service account settings
+#######################################
+
+# variable "aws_alb_serv_account_name" {
+#     type = string
+#     default = "aws-load-balancer-controller"
+# } 
+
+# variable "namespace" {
+#     type = string
+#     default = "kube-system"
+# }
+
+# variable "role_name" {
+#     type = string
+#     default = "AmazonEKSLoadBalancerControllerRole"
+# }
