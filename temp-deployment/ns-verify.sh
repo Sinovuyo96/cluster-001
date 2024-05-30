29a17a62ad4ff76bf1fa402fcda5f84973cadbfb
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-05-30 23:26:16 +0200

    Update and rename xaxs to ns-verify.sh

---
+++ temp-deployment/ns-verify.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+# List of namespaces
+NAMESPACES=("nginx" "argocd")
+
+for NAMESPACE in "${NAMESPACES[@]}"; do
+  if ! kubectl get namespace "$NAMESPACE" >/dev/null 2>&1; then
+    echo "Creating namespace '$NAMESPACE'"
+    kubectl create namespace "$NAMESPACE"
+  else
+    echo "Namespace '$NAMESPACE' already exists. Skipping creation."
+  fi
+done
