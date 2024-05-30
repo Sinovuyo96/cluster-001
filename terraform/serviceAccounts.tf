d29568276826c85ed8f81c7ecbe2b1dd3ee6ca22
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-05-30 19:49:44 +0200

    Update serviceAccounts.tf

--- terraform/serviceAccounts.tf
+++ terraform/serviceAccounts.tf
@@ -1,10 +1,10 @@
 
-# resource "kubernetes_service_account" "aws_load_balancer_controller" {
-#   metadata {
-#     name      = "aws-load-balancer-controller"
-#     namespace = "kube-system"
-#     annotations = {
-#       "eks.amazonaws.com/role-arn" = aws_iam_role.aws_load_balancer_controller.arn
-#     }
-#   }
-# }
+ resource "kubernetes_service_account" "aws_load_balancer_controller" {
+   metadata {
+     name      = var.aws_alb_serv_account_name
+     namespace = var.namespace
+     annotations = {
+       "eks.amazonaws.com/role-arn" = aws_iam_role.aws_load_balancer_controller.arn
+     }
+   }
+ }
