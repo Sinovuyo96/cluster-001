fe17cbafe7241dfd89e604eccd3649079370acdd
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-07-04 22:33:30 +0200

    Update serviceAccounts.tf

--- terraform/serviceAccounts.tf
+++ terraform/serviceAccounts.tf
@@ -4,7 +4,7 @@
       name      = var.aws_alb_serv_account_name
       namespace = var.namespace
       annotations = {
-        "eks.amazonaws.com/role-arn" = aws_iam_role.aws_load_balancer_controller.arn
+        "eks.amazonaws.com/role-arn" = aws_iam_role.eks_load_balancer_controller_role.arn
       }
     }
   }
