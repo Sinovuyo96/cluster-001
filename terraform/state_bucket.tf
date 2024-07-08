3f30bfb85d4077861bc2fe29147d5b90064e91a2
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-07-08 20:55:53 +0200

    Update state_bucket.tf

--- terraform/state_bucket.tf
+++ terraform/state_bucket.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "terraform_state" {
    enabled = true
  }
 
-  Enable server-side encryption by default
+  ##Enable server-side encryption by default
  server_side_encryption_configuration {
    rule {
      apply_server_side_encryption_by_default {
