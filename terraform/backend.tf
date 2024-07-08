268fc8793f81d46981274eb022c8e2d31e924961
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-07-08 21:23:10 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,1 +1,13 @@
 
+##################
+# tf state file  #
+##################
+
+terraform {
+  backend "s3" {
+    bucket         = var.terraform_state_bucket_name
+    key            = "eks/cluster-001/terraform.tfstate"
+    dynamodb_table = "terraform-aws-state-lock"
+    region         = "us-east-1"
+  }
+}
