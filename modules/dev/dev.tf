b5bfadf1ce47b8444d0d0408c2fa81015b36a0c3
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-07-08 20:00:53 +0200

    Update dev.tf

--- modules/dev/dev.tf
+++ modules/dev/dev.tf
@@ -1,6 +1,6 @@
 module "eks-cluster-us-east-1" {
   source                      = "../../terraform/"
-  terraform_state_bucket_name = "temp-terraform-state-bucket-name-2024"
+  terraform_state_bucket_name = "temp-terraform-state-bucket-name-2024-dev"
     
   # Networking configs
   vpc_id                      = "vpc-00030f56014018499"
