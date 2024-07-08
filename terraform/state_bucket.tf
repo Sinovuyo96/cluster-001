39019aabc110f1124154ce54abe0e44b823d7a5f
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-07-08 20:37:31 +0200

    Update state_bucket.tf

--- terraform/state_bucket.tf
+++ terraform/state_bucket.tf
@@ -1,19 +1,19 @@
 ####################
 #terraform State   #
 ####################
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.terraform_state_bucket_name
-  force_destroy = true
-  versioning {
-    enabled = true
-  }
+#resource "aws_s3_bucket" "terraform_state" {
+#  bucket = var.terraform_state_bucket_name
+#  force_destroy = true
+#  versioning {
+#    enabled = true
+#  }
 
   # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
+#  server_side_encryption_configuration {
+#    rule {
+#      apply_server_side_encryption_by_default {
+#        sse_algorithm = "AES256"
+#      }
+#    }
+#  }
+#}
