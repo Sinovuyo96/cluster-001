81743239bd5599242e23b50f2b20b73ee48e7717
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-08-08 21:26:24 +0200

    Update cluster.tf

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -63,5 +63,5 @@ output "endpoint" {
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.dev_cluster.certificate_authority[0].data
+  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
 }
