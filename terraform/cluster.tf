1565132fb3decbece238b7fca4ac60530bc5ad87
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-08-08 23:24:55 +0200

    Update cluster.tf

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -64,8 +64,8 @@ output "endpoint" {
 
 output "kubeconfig-certificate-authority-data" {
   value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
-depends_on = [aws_cloudwatch_log_group.example]
+depends_on = [aws_cloudwatch_log_group.cluster_logging]
 
-  enabled_cluster_log_types = ["api", "audit"]
+  enabled_cluster_log_types = ["api", "audit", "scheduler", "authenticator"]
   name                      = var.cluster_name
 }
